5328428887ee3b271796735e40ade12539932a1e
SMITH RODRIGUEZ <20231123@itla.edu.do>
2025-04-09 16:58:28 -0400

    cambio

--- app/empleo/page.tsx
+++ app/empleo/page.tsx
@@ -1,4 +1,6 @@
 // page.tsx
+"use client"; // Indica que este archivo es un Client Component
+
 import dynamic from 'next/dynamic';
 
 // Cargar dinámicamente el componente con ssr: false para evitar prerenderizado
